fbf4c3c194053892c891cb9580a76b02357ae860
Evgeniy Blinov <zheni-b@yandex.ru>
2024-06-17 13:40:35 +0300

    some change

--- sigmatch/matcher.py
+++ sigmatch/matcher.py
@@ -146,18 +146,13 @@ class SignatureMatcher:
         met_name = False
         met_star = False
         met_double_star = False
-        met_question_mark = False
         all_met_names = set()
 
         for item in expected_signature:
-            if not item.isidentifier() and item not in ('.', '*', '**', '?'):
+            if not item.isidentifier() and item not in ('.', '*', '**'):
                 raise ValueError(f'Only strings of a certain format can be used as symbols for function arguments: arbitrary variable names, and ".", "*", "**", "?" strings. You used "{item}".')
 
             if item == '.':
-                if met_name or met_star or met_double_star or met_question_mark:
-                    raise IncorrectArgumentsOrderError('Positional arguments must be specified first.')
-
-            elif item == '?':
                 if met_name or met_star or met_double_star:
                     raise IncorrectArgumentsOrderError('Positional arguments must be specified first.')
 
