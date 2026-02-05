766ba5d479d2b87e053333680c059fb567258a27
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-05 13:24:15 +0300

    Add ellipsis to all custom exceptions

--- sigmatch/errors.py
+++ sigmatch/errors.py
@@ -1,8 +1,8 @@
 class SignatureMismatchError(Exception):
-    pass
+    ...
 
 class IncorrectArgumentsOrderError(Exception):
-    pass
+    ...
 
 class SignatureNotFoundError(Exception):
-    pass
+    ...
