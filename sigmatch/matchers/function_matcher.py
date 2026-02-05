0b20685dc87e0595d552f744dfef30ba7359e196
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-05 14:53:54 +0300

    Update docstring to specify class name in signature matcher

--- sigmatch/matchers/function_matcher.py
+++ sigmatch/matchers/function_matcher.py
@@ -11,7 +11,7 @@ class FunctionSignatureMatcher(AbstractSignatureMatcher):
     """
 
     def match(self, function: Callable[..., Any], raise_exception: bool = False) -> bool:
-        """We check that the signature of the function passed as an argument corresponds to the "cast" obtained during initialization of the SignatureMatcher object."""
+        """We check that the signature of the function passed as an argument corresponds to the "cast" obtained during initialization of the FunctionSignatureMatcher object."""
         if not callable(function):
             if raise_exception:
                 raise ValueError('It is impossible to determine the signature of an object that is not being callable.')
