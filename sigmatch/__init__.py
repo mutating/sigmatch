0c485c72e70914baf4c37281070f8ed650ea220e
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-06 03:11:26 +0300

    Add PossibleCallMatcher import to __init__.py

--- sigmatch/__init__.py
+++ sigmatch/__init__.py
@@ -1,3 +1,4 @@
 from sigmatch.matchers.function_matcher import FunctionSignatureMatcher as FunctionSignatureMatcher  # noqa: F401
+from sigmatch.matchers.possible_call_matcher import FunctionSignatureMatcher as PossibleCallMatcher  # noqa: F401
 from sigmatch.errors import SignatureMismatchError as SignatureMismatchError  # noqa: F401
 from sigmatch.errors import IncorrectArgumentsOrderError as IncorrectArgumentsOrderError  # noqa: F401
