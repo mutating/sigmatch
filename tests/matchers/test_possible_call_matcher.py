ebcba7826de21e479d7a4e5211b847d10ed20fa8
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-06 15:09:00 +0300

    Add test for star argument detection in call matcher

--- tests/matchers/test_possible_call_matcher.py
+++ tests/matchers/test_possible_call_matcher.py
@@ -2,3 +2,19 @@ import pytest
 from full_match import match
 
 from sigmatch import PossibleCallMatcher
+
+
+def test_there_should_be_star_in_signature_if_call_contains_it():
+    def function_with_star_1(*args): ...
+    def function_with_star_2(a, b, c, *args): ...
+
+    def function_without_star_1(): ...
+    def function_without_star_2(a, b, c): ...
+
+    assert PossibleCallMatcher('*').match(function_with_star_1)
+    assert PossibleCallMatcher('..., *').match(function_with_star_2)
+
+    assert not PossibleCallMatcher('*').match(function_without_star_1, raise_exception=False)
+    assert not PossibleCallMatcher('*').match(function_without_star_2, raise_exception=False)
+    assert not PossibleCallMatcher('..., *').match(function_without_star_2, raise_exception=False)
+    assert not PossibleCallMatcher('.., *').match(function_without_star_2, raise_exception=False)
