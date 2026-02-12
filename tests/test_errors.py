e1da181a9e084265f0f93fa3369e3f38aaa2b5ab
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-12 19:06:01 +0300

    Add test for error class inheritance hierarchy

---
+++ tests/test_errors.py
@@ -0,0 +1,7 @@
+from sigmatch import SignatureError, SignatureMismatchError, SignatureNotFoundError, UnsupportedSignatureError
+
+
+def test_inheritance():
+    assert issubclass(SignatureMismatchError, SignatureError)
+    assert issubclass(SignatureNotFoundError, SignatureError)
+    assert issubclass(UnsupportedSignatureError, SignatureError)
