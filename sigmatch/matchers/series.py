b6a7d8c155d8ca1b494e8806b906060ca4c47ed1
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-04-21 21:33:07 +0300

    Update repr to use describe_call instead of describe_data_object

--- sigmatch/matchers/series.py
+++ sigmatch/matchers/series.py
@@ -1,6 +1,6 @@
 from typing import Any, Callable, Generator, List
 
-from printo import describe_data_object
+from printo import describe_call
 
 from sigmatch import PossibleCallMatcher
 from sigmatch.errors import SignatureMismatchError, SignatureNotFoundError
@@ -20,7 +20,7 @@ class SignatureSeriesMatcher(AbstractSignatureMatcher):
         self.matchers = sorted(set(self.matchers), key=lambda x: x._get_signature_string())
 
     def __repr__(self) -> str:
-        return describe_data_object(type(self).__name__, self.matchers, {})
+        return describe_call(type(self).__name__, self.matchers, {})
 
     def __bool__(self) -> bool:
         return bool(self.matchers)
